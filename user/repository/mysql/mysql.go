1addef4f5b5052cc6f49dea164a4038a507b310c
Folium1 <vampirowitch.vampir@gmail.com>
2023-05-03 22:21:22 +0300

    Fixed some requests

--- user/repository/mysql/mysql.go
+++ user/repository/mysql/mysql.go
@@ -1,10 +1,8 @@
 package repository
 
 import (
-	// "log"
-
-	// "uniLeaks/config"
 	"log"
+
 	"uniLeaks/config"
 	"uniLeaks/models"
 
@@ -26,16 +24,16 @@ func New() *Repository {
 }
 
 // Create creates a new user record in the database.
-func (r Repository) Create(ctx context.Context, newUser models.User) error {
-	result := r.db.WithContext(ctx).Create(newUser)
-	if result.Error != nil {
-		return result.Error
+func (r Repository) Create(ctx context.Context, newUser models.User) (int, error) {
+	err := r.db.WithContext(ctx).Create(&newUser).Last(&newUser).Error
+	if err != nil {
+		return -1, err
 	}
-	return nil
+	return newUser.ID, nil
 }
 
 // GetById returns the user record from the database with the specified ID.
-func (r Repository) GetById(ctx context.Context, id string) (models.User, error) {
+func (r Repository) GetById(ctx context.Context, id int) (models.User, error) {
 	var user models.User
 	result := r.db.WithContext(ctx).Model(&user).Where("id = %v", id).Scan(&user)
 	if result.Error != nil {
@@ -47,7 +45,7 @@ func (r Repository) GetById(ctx context.Context, id string) (models.User, error)
 // GetByMail returns the user record from the database with the specified email address.
 func (r Repository) GetByMail(ctx context.Context, mail string) (models.User, error) {
 	var user models.User
-	result := r.db.WithContext(ctx).Model(&user).Select("id", "password", "banned").Where("email = %v", mail).Scan(&user)
+	result := r.db.WithContext(ctx).Where("email = ?", mail).First(&user)
 	if result.Error != nil {
 		return models.User{}, result.Error
 	}
@@ -55,8 +53,8 @@ func (r Repository) GetByMail(ctx context.Context, mail string) (models.User, er
 }
 
 // BanUser sets the banned flag on the user record with the specified ID.
-func (r Repository) BanUser(ctx context.Context, id string) error {
-	result := r.db.WithContext(ctx).Set("banned", true).Where("id = %v", id)
+func (r Repository) BanUser(ctx context.Context, id int) error {
+	result := r.db.WithContext(ctx).Set("is_banned", true).Where("id = %v", id)
 	if result.Error != nil {
 		return result.Error
 	}
