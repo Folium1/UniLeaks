cc070040512ef68aef779323cce01242c67ab9f9
Folium1 <vampirowitch.vampir@gmail.com>
2023-05-12 22:53:03 +0300

    Provided service for leaks

---
+++ leaks/service/scanFile.go
@@ -0,0 +1,70 @@
+package service
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"os"
+
+	"uniLeaks/leaks"
+)
+
+type scanResult struct {
+	Result bool `json:"CleanResult"`
+}
+
+// scanFile checks file for viruses
+func scanFile(file *os.File) (bool, error) {
+	url := "https://api.cloudmersive.com/virus/scan/file"
+	method := "POST"
+
+	payload := &bytes.Buffer{}
+	writer := multipart.NewWriter(payload)
+	part1,
+		err := writer.CreateFormFile("inputFile", file.Name())
+	_, err = io.Copy(part1, file)
+	if err != nil {
+		log.Println(err)
+		return true, leaks.FileCheckErr
+	}
+	err = writer.Close()
+	if err != nil {
+		log.Println(err)
+		return true, leaks.FileCheckErr
+	}
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
+
+	if err != nil {
+		log.Println(err)
+		return true, leaks.FileCheckErr
+	}
+	req.Header.Add("Content-Type", "multipart/form-data")
+	req.Header.Add("Apikey", os.Getenv("CLOUD_MERSIVE_API"))
+
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	res, err := client.Do(req)
+	if err != nil {
+		log.Println(err)
+		return true, leaks.FileCheckErr
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Println(err)
+		return true, leaks.FileCheckErr
+	}
+	var result scanResult
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		log.Println(err)
+		return true, err
+	}
+	return result.Result, nil
+}
---
+++ leaks/service/usecase.go
@@ -0,0 +1,80 @@
+package service
+
+import (
+	"strconv"
+	"uniLeaks/leaks"
+	"uniLeaks/leaks/repository"
+	"uniLeaks/models"
+)
+
+type Service struct {
+	Repo repository.Repo
+}
+
+func New() leaks.Repository {
+	return Service{repository.New()}
+}
+
+func (s Service) SaveFile(data models.LeakData) error {
+	result, err := scanFile(data.File.OpenedFile)
+	if !result {
+		return leaks.VirusDetectedErr
+	}
+	if err != nil {
+		return err
+	}
+	err = s.Repo.SaveFile(data)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s Service) GetList(data models.SubjectData) ([]models.LeakData, error) {
+	files, err := s.Repo.GetList(data)
+	if err != nil {
+		return nil, err
+	}
+	filesList := make([]models.LeakData, len(files))
+	var file models.LeakData
+	for _, i := range files {
+		file.File.Name = i.Name
+		file.File.Description = i.Description
+		file.File.Id = i.Id
+		file.File.Size = i.Size
+		file.UserData.Dislikes, err = strconv.Atoi(i.Properties["dislikes"])
+		if err != nil {
+			return nil, err
+		}
+		file.UserData.Likes, err = strconv.Atoi(i.Properties["likes"])
+		if err != nil {
+			return nil, err
+		}
+		filesList = append(filesList, file)
+	}
+	return filesList, nil
+}
+
+func (s Service) GetFile(fileId string) ([]byte, error) {
+	b, err := s.Repo.GetFile(fileId)
+	if err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+func (s Service) DislikeFile(fileId string) error {
+	err := s.Repo.DislikeFile(fileId)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s Service) LikeFile(fileId string) error {
+	err := s.Repo.LikeFile(fileId)
+	if err != nil {
+		return err
+	}
+	return nil
+}
