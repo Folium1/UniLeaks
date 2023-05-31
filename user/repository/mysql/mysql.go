c51a56bfdc7bc8247e275c05d02a14093b65f276
Folium1 <vampirowitch.vampir@gmail.com>
2023-05-31 23:58:15 +0300

    New functional

--- user/repository/mysql/mysql.go
+++ user/repository/mysql/mysql.go
@@ -24,7 +24,7 @@ func New() *Repository {
 }
 
 // Create creates a new user record in the database.
-func (r Repository) Create(ctx context.Context, newUser models.User) (int, error) {
+func (r *Repository) Create(ctx context.Context, newUser models.User) (int, error) {
 	err := r.db.WithContext(ctx).Create(&newUser).Last(&newUser).Error
 	if err != nil {
 		return -1, err
@@ -33,7 +33,7 @@ func (r Repository) Create(ctx context.Context, newUser models.User) (int, error
 }
 
 // GetById returns the user record from the database with the specified ID.
-func (r Repository) GetById(ctx context.Context, id int) (models.User, error) {
+func (r *Repository) GetById(ctx context.Context, id int) (models.User, error) {
 	var user models.User
 	result := r.db.WithContext(ctx).Model(&user).Where("id = %v", id).Scan(&user)
 	if result.Error != nil {
@@ -43,7 +43,7 @@ func (r Repository) GetById(ctx context.Context, id int) (models.User, error) {
 }
 
 // GetByMail returns the user record from the database with the specified email address.
-func (r Repository) GetByMail(ctx context.Context, mail string) (models.User, error) {
+func (r *Repository) GetByMail(ctx context.Context, mail string) (models.User, error) {
 	var user models.User
 	result := r.db.WithContext(ctx).Where("email = ?", mail).First(&user)
 	if result.Error != nil {
@@ -52,11 +52,3 @@ func (r Repository) GetByMail(ctx context.Context, mail string) (models.User, er
 	return user, nil
 }
 
-// BanUser sets the banned flag on the user record with the specified ID.
-func (r Repository) BanUser(ctx context.Context, id int) error {
-	result := r.db.WithContext(ctx).Set("is_banned", true).Where("id = %v", id)
-	if result.Error != nil {
-		return result.Error
-	}
-	return nil
-}
