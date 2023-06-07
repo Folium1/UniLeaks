95f46ecd98d7929e80daba3f6bb143762f23be9b
Folium1 <vampirowitch.vampir@gmail.com>
2023-06-07 17:32:50 +0300

    Refactoring

--- leaks/error.go
+++
@@ -1,9 +0,0 @@
-package leaks
-
-import "errors"
-
-var (
-	ErrVirusDetected = errors.New("Знайдений вірус в файлі")
-	ErrFileNotFound     = errors.New("Файл не знайдено")
-	ErrFileCheck     = errors.New("Сталась помилка, на стадії перевірки файлу на віруси")
-)
