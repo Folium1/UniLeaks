60854a04a4b8ac5482e1f9fdd3d7cddb62a80a08
Folium1 <vampirowitch.vampir@gmail.com>
2023-06-08 14:29:36 +0300

    Refactoring

--- auth/handler.go
+++ auth/handler.go
@@ -1,9 +1,18 @@
 package delivery
 
+import (
+	"html/template"
+	userAdmin "uniLeaks/admin/service"
+)
+
 type Handler struct {
+	tmpl        *template.Template
+	userService userAdmin.UserService
 }
 
 // New returns a new instance of the auth handler.
 func New() Handler {
-	return Handler{}
+	tmpl := template.Must(template.ParseGlob("templates/*"))
+	userService := userAdmin.NewUserService()
+	return Handler{tmpl: tmpl, userService: userService}
 }
--- auth/middleware.go
+++ auth/middleware.go
@@ -1,8 +1,10 @@
 package delivery
 
 import (
+	"errors"
 	"log"
 	"net/http"
+	errHandler "uniLeaks/err"
 	"uniLeaks/models"
 
 	"github.com/gin-gonic/gin"
@@ -13,17 +15,17 @@ import (
 // If the refresh token is missing, it deletes cookies and redirects to the login page. If the refresh token is present,
 // It generates a new auth token and saves it. The user ID is then set in the context
 // And the request is passed to the next middleware.
-func (h Handler) AuthAndRefreshMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
+func (h *Handler) AuthAndRefreshMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
 		// Get the auth token from cookies.
-		existedAuth, err := h.authTokenFromCookies(c)
+		existedAuth, err := h.authTokenFromCookies(ctx)
 		if err != nil {
 			// If the auth token is missing, try to refresh the token using the refresh token.
-			existedRefresh, err := h.refreshTokenFromCookies(c)
+			existedRefresh, err := h.refreshTokenFromCookies(ctx)
 			if err != nil {
 				log.Println("Middleware: ", err)
 				// If the refresh token is missing, redirect to the login page.
-				h.handleInvalidToken(http.StatusFound, c)
+				h.handleInvalidToken(http.StatusFound, ctx)
 				return
 			}
 
@@ -31,7 +33,7 @@ func (h Handler) AuthAndRefreshMiddleware() gin.HandlerFunc {
 			if err != nil {
 				log.Println("Middleware: ", err)
 				// If the refresh token is invalid, redirect to the login page.
-				h.handleInvalidToken(http.StatusFound, c)
+				h.handleInvalidToken(http.StatusFound, ctx)
 				return
 			}
 
@@ -39,19 +41,41 @@ func (h Handler) AuthAndRefreshMiddleware() gin.HandlerFunc {
 			newAuthToken := models.Token{TokenType: AuthString, Value: h.generateToken(userId, AuthTokenDuration), Exp: AuthTokenDuration, UserId: userId}
 
 			// Set the user ID in the context and cookies.
-			c.Set("userId", userId)
-			h.SetTokenToCookies(c, newAuthToken)
-			c.Next()
+			ctx.Set("userId", userId)
+			h.SetTokenToCookies(ctx, newAuthToken)
+			ctx.Next()
 			return
 		}
 		// If the auth token is present, validate it. If it is invalid, redirect to the login page. Otherwise, set the user ID in the context and proceed to the next middleware.
 		userId, err := h.validateToken(existedAuth)
 		if err != nil {
 			log.Println("Middleware: ", err)
-			h.handleInvalidToken(http.StatusFound, c)
+			h.handleInvalidToken(http.StatusFound, ctx)
 			return
 		}
-		c.Set("userId", userId)
-		c.Next()
+		ctx.Set("userId", userId)
+		ctx.Next()
+	}
+}
+
+// IsAdmin checks if the user is an admin.
+func (h *Handler) IsAdmin() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		userId, ok := ctx.Get("userId")
+		if !ok {
+			errHandler.ResponseWithErr(ctx, h.tmpl, errHandler.ErrPage, errors.New("Помилка отримання данних"))
+			return
+		}
+		isAdmin, err := h.userService.IsAdmin(userId.(int))
+		if err != nil {
+			errHandler.ResponseWithErr(ctx, h.tmpl, errHandler.ErrPage, errors.New("Помилка отримання данних"))
+			return
+		}
+		if !isAdmin {
+			errHandler.ResponseWithErr(ctx, h.tmpl, errHandler.ErrPage, errors.New("Недостатньо прав"))
+			return
+		}
+		ctx.Set("userId", userId)
+		ctx.Next()
 	}
 }
