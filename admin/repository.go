932de9fc26ae7897757b51558855ed27ad36663c
Folium1 <vampirowitch.vampir@gmail.com>
2023-06-12 20:22:21 +0300

    Provided new functionality

--- admin/repository.go
+++ admin/repository.go
@@ -11,10 +11,12 @@ type DriveRepository interface {
 	FilesList() ([]*drive.File, error)
 	DeleteFile(fileId string) error
 	File(fileID string) (models.LeakData, error)
+	DeleteAllUserFiles(userId string)
 }
 
 type UserRepository interface {
 	BanUser(userId int) error
 	AllUsers() ([]*models.User, error)
 	IsAdmin(ctx context.Context, userId int) (bool, error)
+	GetByNick(ctx context.Context, nick string) (models.User, error)
 }
--- admin/service/user.go
+++ admin/service/user.go
@@ -49,10 +49,46 @@ func (s *UserService) AllUsers() ([]*models.User, error) {
 func (s *UserService) IsAdmin(id int) (bool, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	isAdmin,err := s.repo.IsAdmin(ctx, id)
+	isAdmin, err := s.repo.IsAdmin(ctx, id)
 	if err != nil {
 		logg.Error(fmt.Sprint("Couldn't check if user is admin: ", err))
 		return false, errHandler.UserListReceiveErr
 	}
 	return isAdmin, nil
+}
+
+// GetByNick returns the user record from the database with the specified email address.
+func (s *UserService) GetByNick(nick string) (models.User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	user, err := s.repo.GetByNick(ctx, nick)
+	if err != nil {
+		logg.Error(fmt.Sprint("Couldn't get user by nick: ", err))
+		return user, errHandler.UserListReceiveErr
+	}
+	return user, nil
+}
+
+// GetBannedUsers returns all banned users
+func (s *UserService) GetBannedUsers() ([]*models.User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	users, err := s.repo.GetBannedUsers(ctx)
+	if err != nil {
+		logg.Error(fmt.Sprint("Couldn't get banned users: ", err))
+		return nil, errHandler.UserListReceiveErr
+	}
+	return users, nil
+}
+
+// UnbanUser unbans user
+func (s *UserService) UnbanUser(id int) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err := s.repo.UnbanUser(ctx, id)
+	if err != nil {
+		logg.Error(fmt.Sprint("Couldn't unban user: ", err))
+		return err
+	}
+	return nil
 }
