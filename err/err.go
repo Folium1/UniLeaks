3de73b3572aa9db83bf2a662f55352a0fb0fd4bf
Folium1 <vampirowitch.vampir@gmail.com>
2023-06-14 23:46:49 +0300

    Added new custom errors

--- err/err.go
+++ err/err.go
@@ -2,16 +2,21 @@ package delivery
 
 import (
 	"errors"
+	"fmt"
 	"html/template"
+	"leaks/logger"
 
 	"github.com/gin-gonic/gin"
-	"google.golang.org/appengine/log"
 )
 
 const ErrPage = "errPage.html"
 
+var logg = logger.NewLogger()
+
 // Define errors that can occur during the work of the server
 var (
+	UserIsBannedErr    = errors.New("Мейл заблокований")
+	InvalidActionErr   = errors.New("Невірна дія")
 	VirusDetectedErr   = errors.New("Знайдений вірус в файлі")
 	FileNotFoundErr    = errors.New("Файл не знайдено")
 	FileCheckErr       = errors.New("Сталась помилка, на стадії перевірки файлу на віруси")
@@ -21,13 +26,14 @@ var (
 	BanUserErr         = errors.New("Помилка бану користувача, спробуйте ще раз")
 	UserListReceiveErr = errors.New("Помилка отримання списку користувачів, спробуйте ще раз")
 	FileSaveErr        = errors.New("Помилка збереження файлу, спробуйте ще раз")
+	LikeDislikeErr     = errors.New("Помилка лайку/дизлайку, спробуйте ще раз")
 )
 
 // ResponseWithErr gives a response to user with code 400, in the given page and error message
 func ResponseWithErr(c *gin.Context, t *template.Template, template string, err error) {
 	err = t.ExecuteTemplate(c.Writer, template, err)
 	if err != nil {
-		log.Errorf(c, "Couldn't execute template: %v", err)
+		logg.Error(fmt.Sprintf("Couldn't execute template: %v", err))
 		c.Writer.WriteHeader(500)
 		c.Writer.Write([]byte("Internal server error"))
 	}
