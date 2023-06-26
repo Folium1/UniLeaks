43dcfd491c7fb1b4b26974a889ea4055ec46696a
Vlad <vampirowitch.vampir@gmail.com>
2023-06-26 16:41:48 +0300

    Provided new errors, added IsDuplicateEntryError func

--- err/err.go
+++ err/err.go
@@ -7,6 +7,7 @@ import (
 	"leaks/logger"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-sql-driver/mysql"
 )
 
 const ErrPage = "errPage.html"
@@ -26,8 +27,18 @@ var (
 	BanUserErr         = errors.New("Помилка бану користувача, спробуйте ще раз")
 	UserListReceiveErr = errors.New("Помилка отримання списку користувачів, спробуйте ще раз")
 	FileSaveErr        = errors.New("Помилка збереження файлу, спробуйте ще раз")
-	LikeDislikeErr     = errors.New("Помилка лайку/дизлайку, спробуйте ще раз")
+	LikeDislikeErr     = errors.New("Спробуйте ще раз, сталася помилка")
+	NoUserIdErr        = errors.New("Помилка ідентифікації користувача")
 )
+
+// isDuplicateEntryError checks if the given error is a MySQL duplicate entry error.
+func IsDuplicateEntryError(err error) bool {
+	var mysqlErr *mysql.MySQLError
+	if errors.As(err, &mysqlErr) {
+		return mysqlErr.Number == 1062 // MySQL error code for duplicate entry
+	}
+	return false
+}
 
 // ResponseWithErr gives a response to user with code 400, in the given page and error message
 func ResponseWithErr(c *gin.Context, t *template.Template, template string, err error) {
